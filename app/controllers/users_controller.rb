7fd92a2fa9d82d01e3270a528dd6a115407ffc4a
sabahjones <sabahjones@gmail.com>
2017-03-15 17:10:34 -0500

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,15 @@ class UsersController < ApplicationController
     #User.find
   end
   def loginuser
-
     @users = params[:email]
     render :text => "test"
   end
 
   def create
     user = User.new(name: params[:name], email: params[:email], password: params[:password])
-    if (user.valid?)
-      session[:id] = user.id
-      redirect_to '/posts/index'
-    else
-      redirect_to 'users/login'
+    session[:id] = user.id
+    redirect_to '/posts/index'
     end
-  end
 
 
   def destroy
